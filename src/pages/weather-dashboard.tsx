fa10a9af0e08d7d0d8287029e114829fa2f129d1
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2025-01-23 23:46:45 +0700

    fix: fix bugs project

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -8,6 +8,7 @@ import { CurrentWeather } from '../components/current-weather';
 import { HourlyTemperature } from '../components/hourly-temprature';
 import { WeatherDetails } from '../components/weather-details';
 import { WeatherForecast } from '../components/weather-forecast';
+import { FavoriteCities } from '../components/favorite-cities';
 
 const WeatherDashboard = () => {
     const { coordinates, error: locationError, getLocation, isLoading: locationLoading } = useGeolocation();
@@ -84,7 +85,7 @@ const WeatherDashboard = () => {
 
     return (
         <div>
-            {/* Favorite cities */}
+            <FavoriteCities />
             <div className="flex items-center justify-between">
                 <h1 className="text-xl font-bold tracking-tighter">My location</h1>
                 <Button
