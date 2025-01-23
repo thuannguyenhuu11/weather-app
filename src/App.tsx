2c6b05a656320559e677e6b808c2e57848074b93
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2025-01-23 23:27:07 +0700

    feat: create UI current weather

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,22 @@ import CityPage from './pages/city-page';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 5 * 60 * 1000, // 5 minutes
+            gcTime: 10 * 60 * 1000, // 10 minutes
+            retry: false,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <ThemeProvider defaultTheme="system">
+                <ThemeProvider defaultTheme="dark">
                     <Layout>
                         <Routes>
                             <Route path="/" element={<WeatherDashboard />} />
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,15 +3,22 @@ import { Button } from '../components/ui/button';
 import { useGeolocation } from '../hooks/use-geolocation';
 import WeatherSkeleton from '../components/loading-skeleton';
 import { Alert, AlertDescription, AlertTitle } from '../components/ui/alert';
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from '../hooks/use-weather';
+import { CurrentWeather } from '../components/current-weather';
 
 const WeatherDashboard = () => {
     const { coordinates, error: locationError, getLocation, isLoading: locationLoading } = useGeolocation();
-    console.log(coordinates);
+
+    const weatherQuery = useWeatherQuery(coordinates);
+    const forecastQuery = useForecastQuery(coordinates);
+    const locationQuery = useReverseGeocodeQuery(coordinates);
 
     const handleRefresh = () => {
         getLocation();
         if (coordinates) {
-            //reload weather data
+            weatherQuery.refetch();
+            forecastQuery.refetch();
+            locationQuery.refetch();
         }
     };
 
@@ -50,14 +57,54 @@ const WeatherDashboard = () => {
         );
     }
 
+    const locationName = locationQuery.data?.[0];
+
+    if (weatherQuery.error || forecastQuery.error) {
+        return (
+            <Alert variant="destructive">
+                <AlertCircle className="w-4 h-4" />
+                <AlertTitle>Location Error</AlertTitle>
+                <AlertDescription>
+                    <p>Failed to fetch weather data. Please try again.</p>
+                    <Button onClick={getLocation} variant={'outline'} className="w-fit">
+                        <RefreshCw className="w-4 h-4 mr-2" />
+                        Retry
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        );
+    }
+
+    if (!weatherQuery.data || !forecastQuery.data) {
+        return <WeatherSkeleton />;
+    }
+
     return (
         <div>
             {/* Favorite cities */}
             <div className="flex items-center justify-between">
                 <h1 className="text-xl font-bold tracking-tighter">My location</h1>
-                <Button variant="outline" size="icon" onClick={handleRefresh}>
-                    <RefreshCw className="size-4" />
+                <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={handleRefresh}
+                    disabled={weatherQuery.isRefetching || forecastQuery.isFetching}
+                >
+                    <RefreshCw className={`size-4 ${weatherQuery.isFetching ? 'animate-spin' : ''}`} />
                 </Button>
+            </div>
+
+            {/* Current and Hourly weather */}
+            <div className="grid gap-6">
+                <div>
+                    <CurrentWeather data={weatherQuery.data} locationName={locationName} />
+                    {/* Hourly temprature */}
+                </div>
+
+                <div>
+                    {/* details */}
+                    {/* forecast */}
+                </div>
             </div>
         </div>
     );
